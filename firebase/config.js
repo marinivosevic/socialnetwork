af36ca8cd8d2b3d5753139148c5b6573c3bcd104
marinivosevic <hajdrogedon56@gmail.com>
2023-10-18 17:39:56 +0200

    First commit

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDK6Sr0G3vroF5Kd8df5fE05o6VKCWI2Ng",
+    authDomain: "twitterclone-b784a.firebaseapp.com",
+    projectId: "twitterclone-b784a",
+    storageBucket: "twitterclone-b784a.appspot.com",
+    messagingSenderId: "1069476761131",
+    appId: "1:1069476761131:web:90f0aeb78a7e1188b5a46e",
+    measurementId: "G-FNXFGWY6H2"
+}
+// Initialize Firebase
+let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export default firebase_app;
